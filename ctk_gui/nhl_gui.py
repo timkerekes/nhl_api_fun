e4f9d0f65ca1483ac6e810d26704796eea33c055
Tim <timothy.kerekes@gmail.com>
2023-06-19 14:45:47 -0500

    messing around with layout settings

--- ctk_gui/nhl_gui.py
+++ ctk_gui/nhl_gui.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_appearance_mode("Dark")
 # Themes: "blue" (standard), "green", "dark-blue"
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_default_color_theme("dark-blue")
 
 
 class App(customtkinter.CTk):
@@ -15,15 +15,15 @@ class App(customtkinter.CTk):
         self.title("NHL API")
         self.geometry(f"{1100}x{580}")
 
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure((2, 3), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         # Create Sidebar Frame with App Settings Widgets (turn into navigation later?)
         self.sidebar_frame = customtkinter.CTkFrame(
-            self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+            self, width=100, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(0, weight=1)
 
         self.my_image = customtkinter.CTkImage(light_image=Image.open("./NHL_LOGO.png"),
                                                dark_image=Image.open(
@@ -49,7 +49,7 @@ class App(customtkinter.CTk):
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.set("100%")
-        self.scaling_optionemenu.grid(row=4, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=4, column=0, padx=20, pady=(10, 10))
 
         # Create Searching Options Frame & Widgets
         self.logo_label = customtkinter.CTkLabel(
@@ -61,10 +61,9 @@ class App(customtkinter.CTk):
         self.game_type_optionmenu.grid(row=1, column=1)
 
         # Create Search Output Frame & Widgets
-        self.sidebar_frame = customtkinter.CTkFrame(
-            self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=2, column=1, rowspan=7, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(10, weight=1)
+        self.output_frame = customtkinter.CTkFrame(
+            self, corner_radius=0)
+        self.output_frame.grid(row=2, column=1, columnspan=3, sticky="NSEW")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
